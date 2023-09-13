d8c5c545e84a74c0b00ee058ae26693546390535
Chung Tu <tuthanhtrung2000@gmail.com>
2023-09-13 13:31:05 +0700

    update sidenav

--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -1,9 +1,34 @@
-
+import { NavLink } from 'react-router-dom'
+import './style.scss'
+import { RocketIcon } from '@radix-ui/react-icons'
 
 export default function SideNav() {
     return (
-        <div className="sidenav">
-            sidenav
+        <div className='sidenav'>
+            <NavLink to='/' className="sidenav__item">
+                <span>
+                    <RocketIcon height={14} width={14} />
+                </span>
+                Shared Pockets
+            </NavLink>
+            <NavLink to='/a' className="sidenav__item">
+                <span>
+                    <RocketIcon height={14} width={14} />
+                </span>
+                Shared Pockets
+            </NavLink>
+            <NavLink to='/b' className="sidenav__item">
+                <span>
+                    <RocketIcon height={14} width={14} />
+                </span>
+                Shared Pockets
+            </NavLink>
+            <NavLink to='/c' className="sidenav__item">
+                <span>
+                    <RocketIcon height={14} width={14} />
+                </span>
+                Shared Pockets
+            </NavLink>
         </div>
     )
 }
