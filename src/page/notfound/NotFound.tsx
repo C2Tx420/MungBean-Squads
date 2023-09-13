3c0f7c4959251622c9c9e89d9156e590112df3c3
NhatMy <miketran1406@gmail.com>
2023-09-13 21:23:31 +0700

    notfound

--- src/page/notfound/NotFound.tsx
+++ src/page/notfound/NotFound.tsx
@@ -1,7 +1,23 @@
-
-
+import PublicHeader from "../../components/header/public/PublicHeader";
+import "./NotFound.scss";
+import { Link} from 'react-router-dom';
 export default function NotFound() {
-    return (
-        <div>NotFound</div>
-    )
+  return (
+    <>
+      <PublicHeader />
+      <div className="main404">
+        <img
+          className="imgmungbean"
+          src="/mungbean.png"
+          width={300}
+          height={300}
+        />
+      </div>
+      <div className="txt404">
+         <h3 >404 NOT FOUND</h3> 
+         <p>It's look like you may have taken a wrong turn, Don't worry..It happens to thes most of us</p>
+            <Link to="/">GO BACK TO OUR HOME PAGE</Link>
+        </div>
+    </>
+  );
 }
