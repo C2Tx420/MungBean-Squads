cb5267e63283e46c42395fcc7dc3ce8ecca4cb92
NhatMy <miketran1406@gmail.com>
2023-09-17 08:56:17 +0700

    homefirstpage

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,26 +1,62 @@
-import { Button } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 import { useSquads } from "../../hook/useSquads";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {Section} from "@radix-ui/themes"
+import { FaArrowRight } from 'react-icons/fa';
 import PublicHeader from "../../components/header/public/PublicHeader";
+import './Home.scss'
+import React, { useRef } from "react";
+
+
+
 
 export default function Home() {
-  const { createMultisig, getMultisig, getMainVault } = useSquads();
-  const wallet = useWallet();
-  const { connection } = useConnection()
+ const [showText, setShowText] = useState(false);
 
-  // useEffect(()=>{
-  //   publicKey && getMultisig(connection, publicKey)
-  // },[])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(true);
+    }, 1000);
 
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
-    <>
-      <PublicHeader />
-      <div>
-        <Button onClick={() => wallet.publicKey && createMultisig(wallet)}>create</Button>
-        <Button onClick={() => wallet.publicKey && getMainVault(wallet.publicKey)}>get main vault</Button>
-        <Button onClick={() => wallet.publicKey && getMultisig(connection, wallet.publicKey)}>get</Button>
-        Home
-      </div>
-    </>
+    <div>
+       <PublicHeader />
+           <div className="container">
+            <Section className="section1">
+              <div className="titlemungbean">
+            <h2>Application faster  <br />and better</h2>
+            <p className={`typing-text ${showText ? "show" : ""}`}>
+            Welcome to our website 😉! We are a leading electronic wallet platform. <br /> enabling you to manage digital assets and invest conveniently.<br />Join us now to explore unique investment opportunities through our <br /> electronic wallet.🎉<br />
+            </p>
+
+           <div className="btn"> 
+           <a href="#" className="btnstart">
+           Get Started <FaArrowRight className="arrow-icon" />
+          </a>
+            </div>
+              </div>
+             <div className="imagemungbean">
+              <img src="/mungbean.png" alt=""  className="three-d-image"/>
+             </div>
+            </Section>
+            </div> 
+            
+
+               {/* phan nay la phan 2
+              {/* <div className="seconmain">
+                <div className="img-secon">
+                    <img src="/hihi.jpg" alt="" />
+        
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi!</p>
+                </div>
+
+              </div> */}
+              
+              
+         
+    </div>
   )
 }
--- src/page/notfound/NotFound.tsx
+++ src/page/notfound/NotFound.tsx
@@ -14,8 +14,8 @@ export default function NotFound() {
         />
       </div>
       <div className="txt404">
-         <h3 >404 NOT FOUND</h3> 
-         <p>It's look like you may have taken a wrong turn, Don't worry..It happens to thes most of us</p>
+         <h3 className="notfound" > <span className="sp1">4</span><span className="sp2">0</span><span className="sp3">4</span> NOT FOUND</h3> 
+         <p className="textwarn">It's look like you may have taken a wrong turn, Don't worry..It happens to the most of us</p>
             <Link to="/">GO BACK TO OUR HOME PAGE</Link>
         </div>
     </>
