8386ce8a93f9a05a523329f715b701661a13b0af
Chung Tu <tuthanhtrung2000@gmail.com>
2023-10-09 20:42:41 +0700

    add chart

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -4,48 +4,21 @@ import { PieChart, Pie, Sector, Cell, ResponsiveContainer, AreaChart, CartesianG
 
 const data = [
     {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+      uv: 0,
     },
     {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+      uv: 1,
     },
     {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+      uv: 0.4,
     },
     {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+      uv: 0.5,
     },
     {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-];
+      uv: 1.2,
+    }
+  ];
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 export default function Chart() {
@@ -59,7 +32,42 @@ export default function Chart() {
 
             <Box px="4" pt="3" pb="2">
                 <Tabs.Content value="account">
-                    <p>Make changes to your account.</p>
+                <ResponsiveContainer width='100%' height='40%'>
+              <AreaChart
+                data={data}
+                margin={{ top: 20 }}
+              >
+                <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#00a3ff" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#00a3ff" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <YAxis domain={["auto", "auto"]} orientation="left" />
+                <Tooltip
+                  content={({ active, payload }) => {
+                    if (active && payload && payload.length) {
+                      return (
+                        <div className="dashboard__chart-tooltip">
+                          <span>{payload[0].value} SOL</span>
+                        </div>
+                      )
+                    }
+
+                    return null
+                  }}
+                />
+                <Area
+                  type="monotone"
+                  dataKey="uv"
+                  stroke="#00a3ff"
+                  fillOpacity={1}
+                  strokeWidth={3}
+                  fill="url(#colorUv)"
+                  animateNewValues
+                />
+              </AreaChart>
+            </ResponsiveContainer>
                 </Tabs.Content>
 
                 <Tabs.Content value="documents">
